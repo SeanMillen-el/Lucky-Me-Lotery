eb2a805308ad58a67ab89a0db98866108b6172ce
Sean Millen-el <Sean.millenel1907@gmail.com>
2024-03-22 14:56:54 -0400

    javascript

--- script.js
+++ script.js
@@ -6,7 +6,31 @@ const name = document.getElementById('nameInput').Value.trim()
 nameArray.push(name) 
 displayName()
 nameInput.value = ''
+//check if null
 }
+
+function displayNames(){
+    const nameList = document.getElementById('nameList')//get UL element
+    nameList.innerHTML = ''//clears list
+
+    for (let i = 0; i <nameArray.length; i++){
+        const name = nameArray[i]
+        
+        const li = document.createElement('li')
+        li.className = 'list-group-item'
+
+        const span = document.createElement('span')
+        span.textContent = name
+       
+        li.appendChild(span)
+        
+        nameList.appendChild(li)
+
+
+    }
+
+}
+
 
 document.getElementById('addNameBtn').addEventListener('click', addName)
 
